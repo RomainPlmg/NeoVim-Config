769aef5e1ebed682dd06b241f6ac08ba336a1046
= <=>
2024-05-19 19:38:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# INSTALLATION
+To install the config, simply:
+```bash
+rm -rf .local/share/nvim
+if [[ -d ~/.config/nvim ]]; then
+	mv ~/.config/nvim ~/.config/nvim.back
+fi
+mkdir -p ~/.config/nvim
+git clone https://github.com/RomainPlmg/NeoVim-Config.git ~/.config/nvim
+```
+
+Please understand that this is my personal configuration for my setup. If something doesn't work, feel free to open up an issue or message me, and I will try to help.
+
+**Thank you for your download !**
---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,55 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({})
+		end,
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		config = function()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					"codelldb",
+					"eslint_d",
+					"prettier",
+					"stylua",
+				},
+			})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+				"tsserver",
+				"clangd",
+			},
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+}
